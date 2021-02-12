70e85d62759f83f93a8125e3b0fb8a147aa3f4e9
philip howley <howley.phil@gmail.com>
2021-02-11 21:03:51 -0500

    Add inquirer

--- app.js
+++ app.js
@@ -1,35 +1,21 @@
-// const message = "hello";
-// const sum = 1 + 1;
-// console.log(message);
-// console.log(sum);
-
-// const commandLineArgs = process.argv;
-// console.log(commandLineArgs);
-// console.log(process);
-
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// // console.log(profileDataArgs);
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const generatePage = require("./src/page-template");
 //
-// const printProfileData = (profileDataArr) => {
-//   for (let i = 0; i < profileDataArr.length; i++) {
-//     console.log(profileDataArr[i]);
-//   }
-// };
+// const pageHTML = generatePage(name, github);
 //
-// printProfileData(profileDataArgs);
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
+//
+//   console.log("Portfolio complete! Check out index.html to see the output");
+// });
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const printProfileData = (profileDataArr) => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log("================");
-
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));

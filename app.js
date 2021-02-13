b0fcd5c0bcd9080839543aaca7cc4f1d52670de4
philip howley <howley.phil@gmail.com>
2021-02-12 22:59:39 -0500

    Add recursive function to build array of projects using conditional logic

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const inquirer = require("inquirer");
 // const fs = require("fs");
 // const generatePage = require("./src/page-template");
 //
@@ -10,12 +9,87 @@ const inquirer = require("inquirer");
 //   console.log("Portfolio complete! Check out index.html to see the output");
 // });
 
-inquirer
-  .prompt([
+const inquirer = require("inquirer");
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: "input",
       name: "name",
       message: "What is your name?",
     },
-  ])
-  .then((answers) => console.log(answers));
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+    },
+  ]);
+};
+
+// if there's no 'projects' array property, create one
+const promptProject = (portfolioData) => {
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+
+  return (
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the name of your project?",
+        },
+        {
+          type: "input",
+          name: "description",
+          message: "Provide a description of the project (Required)",
+        },
+        {
+          type: "input",
+          name: "link",
+          message: "Enter the Github link to your project (Required)",
+        },
+        {
+          type: "confirm",
+          name: "feature",
+          message: "Would you like to feature this project?",
+          default: false,
+        },
+        {
+          type: "confirm",
+          name: "confirmAddProject",
+          message: "Would you like to enter another project?",
+          default: false,
+        },
+      ])
+      // if they want to add another project
+      .then((projectData) => {
+        portfolioData.projects.push(projectData);
+        // does the user want to add another project?
+        if (projectData.confirmAddProject) {
+          // yes i want to add another project
+          return promptProject(portfolioData);
+        } else {
+          // no more projects to add
+          return portfolioData;
+        }
+      })
+  );
+};
+
+promptUser()
+  .then(promptProject)
+  .then((portfolioData) => {
+    console.log(portfolioData);
+  });
